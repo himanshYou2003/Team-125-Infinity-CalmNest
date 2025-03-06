0a33f4f5ad59b343a9f37b0556603cac930f07f5
himanshu <himanshukumar935437#gmail.com>
2025-03-06 10:14:13 +0530

    last

--- backend/server.js
+++ backend/server.js
@@ -69,3 +69,6 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
+
+
+export default app;
